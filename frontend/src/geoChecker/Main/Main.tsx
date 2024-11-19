a023aa58af9f9cd60437acb4b6f4ed3b59e903d4
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-11-19 10:18:00 +0100

    Create Main page component

---
+++ frontend/src/geoChecker/Main/Main.tsx
@@ -0,0 +1,19 @@
+import "./Main.css";
+
+const Main = (): React.ReactElement => {
+  return (
+    <ul className="main-container">
+      <li>
+        <a href="">Empleado</a>
+      </li>
+      <li>
+        <a href="">Supervisor</a>
+      </li>
+      <li>
+        <a href="">Administrador</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Main;
