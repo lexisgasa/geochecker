48e6fb71f656b34c3606d1da36173f91ae484ecc
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-12-04 17:38:44 +0100

    Create NotFound page

--- frontend/src/components/router/router.tsx
+++ frontend/src/components/router/router.tsx
@@ -5,11 +5,13 @@ import {
 } from "react-router";
 import App from "../App/App";
 import Main from "../../geoChecker/pages/Main/Main";
+import NotFound from "../../geoChecker/pages/NotFound/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Main />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
---
+++ frontend/src/geoChecker/pages/NotFound/NotFound.tsx
@@ -0,0 +1,5 @@
+const NotFound = (): React.ReactElement => {
+  return <h2>La página que buscas no existe..</h2>;
+};
+
+export default NotFound;
